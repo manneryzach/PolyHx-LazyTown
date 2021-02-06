fed2e72ad63bc4c8b68f5eff08bf7dfbcf54ddcf
ManneryZach <zachary.et.mann@gmail.com>
2021-02-06 00:28:43 -0500

    commit

--- HackatownBackend/src/Carpool.java
+++ HackatownBackend/src/Carpool.java
@@ -48,8 +48,13 @@ public class Carpool {
         // Separating drivers and passengers IDs
         ArrayList<Integer> driverIDs = new ArrayList<>();
         ArrayList<Integer> passengerIDs = new ArrayList<>();
+
+        double cutoff = 0.5; // TODO find right cutoff value
+
+        ArrayList<UserPair> pairs = new ArrayList<>();
+
         for (int key : users.keySet()){
-            if (users.get(key).isPassenger){
+            if (((users.get(key)).isPassenger) == true){
                 passengerIDs.add(key);
             }else{
                 driverIDs.add(key);
@@ -57,20 +62,52 @@ public class Carpool {
         }
         // Binary search to find least distance between driver path and passenger coords
         ArrayList<Point> d_path;
-        int L;
-        int R;
-        int m;
+        int LA = 0;
+        int LB = 0;
+        int RA = 0;
+        int RB = 0;
+        int mA = 0;
+        int mB = 0;
+        double pA_norm;
+        double pB_norm;
         for (Integer p_key : passengerIDs){
+            pA_norm = eucNorm(users.get(p_key).coordA);
+            pB_norm = eucNorm(users.get(p_key).coordB);
             for (Integer d_key : driverIDs){
                 d_path = map.shortestPath(users.get(d_key).coordA, users.get(d_key).coordB);
-                L = 0;
-                R = d_path.size() - 1;
-                while (L <= R){
-                    m = 1;
+                Sorting.quickSort(d_path, 0, d_path.size() - 1);
+                LA = 0;
+                LB = 0;
+                RA = d_path.size() - 1;
+                RB = RA;
+                // Binary search coord A
+                while (LA <= RA){
+                    mA = (int) ((LA+RA)/2);
+                    if (d_path.get(mA) > pA_norm){
+                        RA = mA - 1;
+                    }else if(d_path.get(mA) < pA_norm){
+                        LA = mA + 1;
+                    }else{
+                        break;
+                    }
+                }
+                // Binary search coord B
+                while (LB <= RB){
+                    mB = (int) ((LB+RB)/2);
+                    if (d_path.get(mB) > pB_norm){
+                        RA = mA - 1;
+                    }else if(d_path.get(mB) < pB_norm){
+                        LB = mB + 1;
+                    }else{
+                        break;
+                    }
+                }
+                if (eucDist(users.get(p_key).coordA, d_path.get(mA)) < cutoff && eucDist(users.get(p_key).coordB, d_path.get(mB)) < cutoff){
+                    pairs.add(new UserPair(p_key, d_key));
                 }
             }
         }
-        return null;
+        return pairs;
     }
 
     // Finds the best path for each user (needs to be adapted to optimize for every user)
