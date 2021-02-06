5a7becf181e09c0579114e93df7a7e90bd946366
Carl Kristof Tessier <carlkristoftessier@gmailcom>
2021-02-06 13:51:33 -0500

    :)

--- HackatownBackend/src/HackatownTests.java
+++ HackatownBackend/src/HackatownTests.java
@@ -1,5 +1,5 @@
 import DataTypes.Point;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 import java.awt.Canvas;
@@ -8,14 +8,14 @@ import javax.swing.JFrame;
 
 class HackatownTests extends Canvas{
 
-    void pointSorting() {
+    static void pointSorting() {
         ArrayList<Point> points = new ArrayList<>();
 
         points.add(new Point(3.0, 4.0));
         points.add(new Point(6.0, 8.0));
         Sorting.quickSort(points, 0, 1);
 
-        System.out.println(points);
+        // System.out.println(points);
 
         RoadNetwork map = new RoadNetwork("./data/routes.json");
         ArrayList<DataTypes.Point> coords = map.getSortedCoordinates();
@@ -23,10 +23,15 @@ class HackatownTests extends Canvas{
         for (Point p : coords) {
             norms.add(Point.eucNorm(p));
         }
-        System.out.println(norms);
+        //System.out.println(norms);
         for (int i = 1; i < norms.size(); i++) {
             if (norms.get(i - 1) > norms.get(i))
                 throw new RuntimeException("Array is not properly sorted at point" + coords.get(i));
         }
+        System.out.println("Success!");
+    }
+
+    public static void main(String[] args) {
+        pointSorting();
     }
 }
