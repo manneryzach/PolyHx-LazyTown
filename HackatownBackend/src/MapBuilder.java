bb829a71f878a98551d198e8ad3e552f0ceccbc9
Carl Kristof Tessier <carlkristoftessier@gmailcom>
2021-02-06 15:57:06 -0500

    :)

--- HackatownBackend/src/MapBuilder.java
+++ HackatownBackend/src/MapBuilder.java
@@ -39,6 +39,36 @@ public class MapBuilder {
                     // Update road coordinates in next for loop instead
                     Road road = new Road(name, direction, road_speed);
 
+                    Iterator iter = coords.iterator();
+
+                    Object p0_temp =  iter.next();
+                    Point p0 = new Point((Double) ((JSONArray) p0_temp).get(0), (Double) ((JSONArray) p0_temp).get(1));
+
+                    Object p1_temp = iter.next();
+                    Point p1 = new Point((Double) ((JSONArray) p1_temp).get(0), (Double) ((JSONArray) p1_temp).get(1));
+
+                    while(iter.hasNext()) {
+                        if (direction >= 0) {
+                            if (network.get(p0) == null) {
+                                ArrayList<Road> hash_roads = new ArrayList<>();
+                                hash_roads.add(new Road(name, p0, p1, road_speed));
+                                network.put(p0, hash_roads);
+                            } else network.get(p0).add(new Road(name, p0, p1, road_speed));
+                        }
+                        if (direction <= 0) {
+                            if (network.get(p1) == null) {
+                                ArrayList<Road> hash_roads = new ArrayList<>();
+                                hash_roads.add(new Road(name, p1, p0, road_speed));
+                                network.put(p1, hash_roads);
+                            } else network.get(p1).add(new Road(name, p1, p0, road_speed));
+                        }
+                        // Update pointers
+                        DataTypes.Point temp = new DataTypes.Point(p1.x, p1.y);
+                        p1_temp = iter.next();
+                        p1 = new Point((Double) ((JSONArray) p1_temp).get(0), (Double) ((JSONArray) p1_temp).get(1));
+                        p0 = temp;
+                    }
+
                     for (Object elem : coords) {
                         Point p = new Point((Double) ((JSONArray) elem).get(0), (Double) ((JSONArray) elem).get(1));
                         road.getCoordinates().add(p);
