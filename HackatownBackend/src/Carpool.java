6747c109e4e37a1184e0077b960eb1e099661f90
Carl Kristof Tessier <carlkristoftessier@gmailcom>
2021-02-05 19:37:00 -0500

    :)

--- HackatownBackend/src/Carpool.java
+++ HackatownBackend/src/Carpool.java
@@ -2,8 +2,7 @@ import java.util.*;
 
 public class Carpool {
     /** Fields **/
-    private HashMap<Double[], ArrayList<Road>> roads;
-    private MapBuilder parser;
+
 
     /** Constructors **/
     public Carpool() {
--- HackatownBackend/src/RoadNetwork.java
+++ HackatownBackend/src/RoadNetwork.java
@@ -1,2 +1,25 @@
+import java.util.*;
+
 public class RoadNetwork {
+    /** Fields **/
+    private HashMap<Double[], ArrayList<Road>> roads;
+    private MapBuilder parser;
+
+    /** Constructors **/
+    public RoadNetwork(String JSONPath) {
+
+    }
+
+    /** accessors **/
+    public ArrayList<Road> getRoad(Double[] coordinates) {
+        return roads.get(coordinates);
+    }
+
+    /** Mutators **/
+
+    /** Custom methods **/
+    public ArrayList<Road> shortestPath(Double[] coordA, Double[] coordB) {
+        return null;
+    }
+
 }
