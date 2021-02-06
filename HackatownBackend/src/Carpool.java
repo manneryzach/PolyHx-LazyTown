0c1eab0b57e999d8a598c02c0c09ab55bbe226ca
ManneryZach <zachary.et.mann@gmail.com>
2021-02-05 19:03:09 -0500

    Removed GeoBaseParser

---
+++ HackatownBackend/src/Carpool.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Carpool {
+    /** Fields **/
+    private HashMap<Double[], ArrayList<Road>> roads;
+    private MapBuilder parser;
+
+    /** Constructors **/
+    public Carpool() {
+
+    }
+
+    /** **/
+
+    /** **/
+
+    /** **/
+
+}
